58d394436c70f93f17bec21aaaa7bdc4e89df44f
disappear <disappear123@mail.ru>
2022-12-19 14:40:09 +0300

    fix: починил возведение в отрицательную степень

--- Lesson4/Program.cs
+++ Lesson4/Program.cs
@@ -2,30 +2,7 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16 */
 
-/* int MyPow(int base1, int power1) 1 Вариант
-{
-	// int result = 1;
-	if (power1 > 0)
-	{
-		int result = 1;
-		for (int i = 0; i < power1; i++)
-		{
-			result *= base1; // result = result * base1;
-		}
-		return result;
-	}
-	if (power1 < 0)
-	{
-		int result = 0;
-		for (int i = 0; i < power1; i++)
-		{
-			result /= result;
-		}
-		return result;
-	}
-	return 1;
-} */
-int MyPow(int base1, int power1)
+/* int MyPow(int base1, int power1)
 {
 	int res = 1;
 	for (int i = 0; i < Math.Abs(power1); i++)
@@ -38,20 +15,38 @@ int MyPow(int base1, int power1)
 	}
 	else
 	{
-		return 1 / res;
+		int res2 = 1 / res;
+		return res2;
+	}
+} */
+double MyPow(int base1, int power1)
+{
+	double res = 1;
+	for (int i = 0; i < Math.Abs(power1); i++)
+	{
+		res *= base1;
+	}
+	if (power1 >= 0)
+	{
+		return res;
+	}
+	else
+	{
+		double res2 = 1 / res;
+		return res2;
 	}
 }
 System.Console.WriteLine("1 вывод: " + MyPow(3, 5) + " <- положительная степень");
 System.Console.WriteLine("2 вывод: " + MyPow(2, 4) + " <- положительная степень");
 System.Console.WriteLine("3 вывод: " + MyPow(7, 0) + " <- нулевая степень");
-System.Console.WriteLine("4 вывод: " + MyPow(510, -7) + " <- отрицательная степень");
+System.Console.WriteLine("4 вывод: " + MyPow(4, -5) + " <- отрицательная степень");
 
 /* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 452 -> 11
 82 -> 10
 9012 -> 12 */
 
-void Sum()
+/* void Sum()
 {
 	System.Console.Write("Введите число: ");
 	int number = int.Parse(Console.ReadLine());
@@ -66,13 +61,13 @@ void Sum()
 for (int i = 0; i < 3; i++)
 {
 	Sum();
-}
+} */
 
 /* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33] */
 
-void ArrFromConsole()
+/* void ArrFromConsole()
 {
 	System.Console.Write("Задайте размер массива: "); // 8
 	int size = int.Parse(Console.ReadLine());
@@ -97,4 +92,4 @@ void PrintArray(int[] array)
 	Console.Write("]");
 }
 
-ArrFromConsole();
+ArrFromConsole(); */
